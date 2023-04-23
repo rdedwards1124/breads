07036546a07954fc6e3ce9b476ff6791d67d4c3c
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-23 12:42:41 -0700

    commit-3

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const breads = express.Router()
+const Bread = require('../models/bread.js')
 
 // Index
 breads.get("/", (req,res)=>{
-    res.send("This is index at /breads")
+    res.send(Bread)
+})
+
+// Show: Read one?
+breads.get("/:arrayIndex", (req,res)=>{
+    const arrayIndex = req.params.arrayIndex
+    res.send(Bread[arrayIndex])
 })
 
 // Exports
 module.exports = breads
+
 
